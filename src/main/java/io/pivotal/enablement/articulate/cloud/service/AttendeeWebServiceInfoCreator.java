cb9b42db0248b8e5bf994c5087af2ca592bd9cae
Dave Roberts <droberts@gopivotal.com>
2016-03-30 10:05:41 -0500

    removing tag logic

--- src/main/java/io/pivotal/enablement/articulate/cloud/service/AttendeeWebServiceInfoCreator.java
+++ src/main/java/io/pivotal/enablement/articulate/cloud/service/AttendeeWebServiceInfoCreator.java
@@ -2,25 +2,14 @@ package io.pivotal.enablement.articulate.cloud.service;
 
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.cloudfoundry.CloudFoundryServiceInfoCreator;
 import org.springframework.cloud.cloudfoundry.Tags;
 
 public class AttendeeWebServiceInfoCreator extends
 		CloudFoundryServiceInfoCreator<WebServiceInfo> {
 
-	public static final String ATTENDEE_SERVICE_TAG = "attendee-service";
-
 	public AttendeeWebServiceInfoCreator() {
-		super(new Tags(ATTENDEE_SERVICE_TAG));
-	}
-
-	@Override
-	public boolean accept(Map<String, Object> serviceData) {
-		Map<String, Object> credentials = getCredentials(serviceData);
-		String tag = (String) credentials.get("tag");
-		return super.accept(serviceData) || ATTENDEE_SERVICE_TAG.equals(tag);
+		super(new Tags(),"http");
 	}
 
 	@Override
@@ -28,7 +17,7 @@ public class AttendeeWebServiceInfoCreator extends
 		String id = (String) serviceData.get("name");
 
 		Map<String, Object> credentials = getCredentials(serviceData);
-		String uri = getStringFromCredentials(credentials, "uri", "url");
+		String uri = getUriFromCredentials(credentials);
 
 		return new WebServiceInfo(id, uri);
 	}
